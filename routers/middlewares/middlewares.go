5df0048db62708a77ba09d4288137929a69e88a8
Alireza <a.r.paragon@gmail.com>
2024-02-19 21:54:09 +0330

    added middleware

--- routers/middlewares/middlewares.go
+++ routers/middlewares/middlewares.go
@@ -1,12 +1,22 @@
 package middlewares
 
 import (
+	"fmt"
 	"net/http"
+	"reflect"
+	"strconv"
 	"strings"
 
 	"github.com/alireza-mf/go-ecommerce/util/jwt"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
+
+type Input struct {
+	Params any
+	Body   any
+	Query  any
+}
 
 func Authorize() gin.HandlerFunc {
 	return func(context *gin.Context) {
@@ -31,5 +41,68 @@ func Authorize() gin.HandlerFunc {
 			return
 		}
 		context.Next()
+	}
+}
+
+func ValidateRequest[TInput any]() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		// tinput := new(TInput)
+		var tinput TInput
+		input := (any)(tinput).(Input)
+		params, _ := reflect.TypeOf(input).FieldByName("Params")
+		body, _ := reflect.TypeOf(input).FieldByName("Body")
+		query, _ := reflect.TypeOf(input).FieldByName("Query")
+
+		// get the type of the struct
+		//inputType := reflect.TypeOf(Input{})
+
+		// >> params
+		if params.Type.NumField() != 0 {
+			for _, field := range reflect.VisibleFields(reflect.TypeOf(params)) {
+				param := reflect.ValueOf(&params).Elem().FieldByName(field.Name)
+				paramValue := c.Param(field.Tag.Get("json"))
+				fmt.Println(param)
+				fmt.Printf("%+v\n", field)
+				fmt.Println(field.Name)
+				fmt.Println(field.Tag.Get("json"), paramValue)
+
+				// it can be either string or int, we should check if can be convertible to int
+				if field.Type.Kind().String() == "string" {
+					param.SetString(paramValue)
+				} else {
+					intParam, err := strconv.ParseInt(paramValue, 10, 64)
+					if err != nil {
+						c.JSON(http.StatusBadRequest, gin.H{"error": "Param is not in number format."})
+						return
+					}
+					param.SetInt(intParam)
+				}
+			}
+		}
+
+		fmt.Println(body.Type.NumField())
+		// >> body
+		if body.Type.NumField() != 0 {
+			err := c.ShouldBindBodyWith(&input.Body, binding.JSON)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
+			}
+			fmt.Printf("%+v\n", reflect.ValueOf(&body))
+		}
+
+		// >> query
+		if query.Type.NumField() != 0 {
+			err := c.ShouldBindQuery(&query)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
+			}
+		}
+
+		// input = {{} {},  {}}
+		fmt.Printf("%+v\n", input)
+		c.Set("input", input)
+		c.Next()
 	}
 }
